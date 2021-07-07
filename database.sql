c08e126e466b705823c8ffe835cd87b8136709b9
theRightHooprod <60564900+theRightHooprod@users.noreply.github.com>
2021-07-07 05:26:08 -0500

    updated index.html

--- database.sql
+++ database.sql
@@ -111,5 +111,21 @@ CREATE TABLE shopping(
 	FOREIGN KEY (uuid) REFERENCES users (id),
 	FOREIGN KEY (proid) REFERENCES products (id)
 );
-
-
+--REGISTROS 
+--producers
+INSERT INTO producers (id,p_name,p_desc, city)
+VALUES ('123','cafe organico MAJOMUT','En su sabor se aprecian notas a chocolate y avellana, de suave acidez, esto como
+resultado de la presencia de granos arábigos le confieren atributos en taza','Altos de Chiapas')
+INSERT INTO producers (id,p_name,p_desc, city)
+VALUES ('124','Alcoholatura de Ajo','Antiséptico, expectorante, reduce niveles de colesterol, antihistamínico natural ')
+--categories
+INSERT INTO categories (id,c_name)
+values('1','CAFE MOLIDO')
+INSERT INTO categories (id,c_name)
+values('2','HERBOLARIA')
+--products
+INSERT INTO  products (id,cid,porid,p_name,price,p_desc,stock)
+VALUES ('10','1','cafe organico MAJOMUT','$200','En su sabor se aprecian notas a chocolate y avellana, de suave acidez, esto como
+resultado de la presencia de granos arábigos le confieren atributos en taza','200')
+INSERT INTO  products (id,cid,porid,p_name,price,p_desc,stock)
+VALUES ('11','2','Alcoholatura de Ajo','$100','Antiséptico, expectorante, reduce niveles de colesterol, antihistamínico natural.','200')
