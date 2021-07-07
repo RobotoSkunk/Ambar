ffb160c243b16dd63b1bd6406ae147c2803f3e7c
RobotoSkunk <49355293+RobotoSkunk@users.noreply.github.com>
2021-07-07 02:29:04 -0500

    sessions.php finished

--- src/ambar/api/test/zapato.js
+++ src/ambar/api/test/zapato.js
@@ -1,7 +1,16 @@
 jQuery(function() {
 	var fd = new FormData();
-	fd.append('action', 'end-session');
-	fd.append('token', 'end-session');
+	fd.append('action', 'signup');
+	fd.append('u_name', 'end-session');
+	fd.append('u_lastname', 'end-session');
+	fd.append('email', 'end-session');
+	fd.append('pwrd', 'end-session');
+	fd.append('direction', 'end-session');
+	fd.append('cp', 'end-session');
+	fd.append('telephone', 'end-session');
+	fd.append('birthdate', '2001-10-03');
+
+	// $_POST['u_name'], $_POST['u_lastname'], $_POST['email'], $_POST['pwrd'], $_POST['direction'], $_POST['cp'], $_POST['telephone'], $_POST['birthdate']
 
 	$.ajax('../sessions.php', {
 		"data": fd,
